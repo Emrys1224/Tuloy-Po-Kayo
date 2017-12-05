782b325a04495e3e616181ff346b50691d1e9884
Emrys1224 <jamerec1224@gmail.com>
2017-12-05 13:29:41 +0800

    make the title generated dynamically; put the custom styles in the page file

--- includes/head.php
+++ includes/head.php
@@ -4,12 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Tuloy Po Kayo - Home</title>
+	<title>Tuloy Po Kayo - <?php echo $pageTitle; ?></title>
 	
 	<?php include 'includes/lib.php'; ?>
 	
 	<link href="css/common-blue.css" rel="stylesheet" type="text/css">
-	<link href="css/signup-login-modal.css" rel="stylesheet" type="text/css">
-	<link href="css/index.css" rel="stylesheet" type="text/css">
-</head>
-<body>
