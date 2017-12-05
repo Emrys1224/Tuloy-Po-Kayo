e0231505b3ec2b2a46511e467155e68de535962a
Emrys1224 <jamerec1224@gmail.com>
2017-12-05 10:24:09 +0800

    include account type on retrieving account details

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ require_once 'core/init.php';
 
 // account details
 $id = 0;
-$username = "Ngalan";
+$username = "";
+$acctType = "";
 
 if (array_key_exists("logout", $_GET)) {
     // Clear _SESSION and _COOKIE
@@ -27,7 +28,9 @@ if (array_key_exists("id", $_COOKIE) AND $_COOKIE['id']) {
 if (array_key_exists("id", $_SESSION) AND $_SESSION['id']) {
 	// set account details
 	$id = $_SESSION['id'];
-	$username = $db->query("SELECT `firstname` FROM `account` WHERE `id` = '$id'")->fetchColumn();
+	$acctDetails = $db->query("SELECT `firstname`, `status` FROM `account` WHERE `id` = '$id'")->fetch();
+	$username = $acctDetails['firstname'];
+	$acctType = $acctDetails['status'];
 
 	// test only
 	$testArr["session_id"] = $id;
