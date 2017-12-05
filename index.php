fa1ca5c096afa0bea9579d79d6648a01b168528d
Emrys1224 <jamerec1224@gmail.com>
2017-12-05 13:02:41 +0800

    put the checking of _SESSION and _COOKIE, and account details retrieval in a separate file

--- index.php
+++ index.php
@@ -5,36 +5,7 @@ session_start();
 $testArr = array();
 
 require_once 'core/init.php';
-
-// account details
-$id = 0;
-$username = "";
-$acctType = "";
-
-if (array_key_exists("logout", $_GET)) {
-    // Clear _SESSION and _COOKIE
-    unset($_SESSION['id']);
-    $_COOKIE["id"] = "";
-    setcookie("id", false, time() - 60*60, "/");
-}
-
-if (array_key_exists("id", $_COOKIE) AND $_COOKIE['id']) {
-	$_SESSION['id'] = $_COOKIE['id'];
-
-	// test only
-	$testArr["coockie_id"] = $_COOKIE['id'];
-}
-
-if (array_key_exists("id", $_SESSION) AND $_SESSION['id']) {
-	// set account details
-	$id = $_SESSION['id'];
-	$acctDetails = $db->query("SELECT `firstname`, `status` FROM `account` WHERE `id` = '$id'")->fetch();
-	$username = $acctDetails['firstname'];
-	$acctType = $acctDetails['status'];
-
-	// test only
-	$testArr["session_id"] = $id;
-}
+require_once 'includes/acct-details.php';
 
 include 'includes/head.php';
 
@@ -125,8 +96,6 @@ include 'includes/nav.php';
 <?php include 'includes/footer.php'; ?>
 
 <!-- for test oonly -->
-
-
 <script type="text/javascript">
 	// select a tab (signup-login-modal)
 	// $('#signup-login-tabs a').click(function (e) {
