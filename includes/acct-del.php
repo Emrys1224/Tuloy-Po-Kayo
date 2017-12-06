fa11c29c69af7b5dd1b973f6f743977070390292
Emrys1224 <jamerec1224@gmail.com>
2017-12-06 20:35:05 +0800

    created acct-del.php (account delete modal)

---
+++ includes/acct-del.php
@@ -0,0 +1,34 @@
+<!-- delete account confirmation modal -->
+<div class="modal fade" id="del-acct-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      </div>
+
+      <div class="modal-body">
+        <p class="lead">Are you sure you want to delete your account?</p>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" id="btn-del-acct">Delete</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+      </div>
+
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!-- account deleted confirmation modal -->
+<div class="modal fade" id="acct-delted-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body" id="del-confirmation">
+        <p class="lead">Your account has been deleted.</p>
+        <p class="lead">Thank you for using our services.</p>
+      </div>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
