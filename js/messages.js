e4d2c8ec530ead9e69a4854545c7a7b4ebe12762
Emrys1224 <jamerec1224@gmail.com>
2017-12-14 22:06:22 +0800

    changed the return type of $.post to json and add code for displaying the messages

--- js/messages.js
+++ js/messages.js
@@ -3,16 +3,18 @@ $('#messages-view').hide()
 
 // view conversations
 $('#messages-list h4').on('click', function() {
-	var convId = $(this).parent().data('conv_id');
-	console.log(convId);
+	// set the text for #msgs-header
+	$('#msgs-header h4').text($(this).text());
 
+	// get conversation Id
+	var convId = $(this).parent().data('conv_id');
+
+	// get and display the messages for the coresponding conversation
 	getMessages({convId: convId});
 
 	toggleView($('#messages-list'), $('#messages-view'), function() {
 		$('#msgs-body').scrollTop($('#msgs-body').prop('scrollHeight'))
 	});
-
-	console.log('ok1');
 })
 
 // close conversations
@@ -20,12 +22,27 @@ $('#close-btn').on('click', function() {
 	toggleView($('#messages-view'), $('#messages-list'));
 })
 
-function getMessages(convWith) {
+// get messages from database with ajax
+function getMessages(convId) {
 	var url = "helpers/get-messages.php";
 
-	$.post(url, convWith, function(response) {
-		resp = response;
+	// save result in an object and display accordingly
+	$.post(url, convId, function(response) {
+		var messages = response;
 
-		alert(response);
-	});
+		// remove the previus content of #msgs-body
+		$('#msgs-body').html('');
+
+		// display the messages
+		for (var msg in messages) {
+			dispMessages(messages[msg]);
+		}
+
+	}, "json");
+}
+
+// add/display a message
+function dispMessages(message) {
+	var dispMsg = '<div class="clearfix"><p class="'+message['sender']+'">'+message['content']+'</p></div>';
+	$('#msgs-body').append(dispMsg);
 }
