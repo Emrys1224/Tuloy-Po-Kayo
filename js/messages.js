4f23ed84f6be69c2b3ef0e691a9c91a773432828
Emrys1224 <jamerec1224@gmail.com>
2017-12-18 22:39:22 +0800

    added timestamp of the messages changed the way the messages are added

--- js/messages.js
+++ js/messages.js
@@ -11,55 +11,46 @@ Array.min = function( array ){
 
 // get messages from database with ajax
 // @param reference of where the entry should start(conversation_id and/or message.id)
-// @param pos(position) of where the messages is to be appended ("after" for new display; "before" for loading the additional previous messages)
-function getMessages(reference, pos) {
+function getMessages(reference) {
 	var url = "helpers/get-messages.php";
 
 	// save result in an object and display accordingly
 	$.post(url, reference, function(messages) {
 		var msgIds = Object.keys(messages);
-		// update oldestMsgId if array is not empty
-		msgIds.length > 0 ? oldestMsgId = Array.min(msgIds) : null;
+		// track the oldest message
+		oldestMsgId = msgIds.length > 0 ? Array.min(msgIds) : null;
 
-		dispMessages(messages, pos);
+		dispMessages(messages);
 	}, "json");
 }
 
 // add/display a message
 // @param messages array of messages
-// @param pos(position) of where the messages is to be appended ("after" for new display; "before" for loading the additional previous messages)
-function dispMessages(messages, pos) {
-	var dispMsg = '';
-
+function dispMessages(messages) {
 	if (!jQuery.isEmptyObject(messages)) {
-		// build html
-		for (var msg in messages) {
-			var message = messages[msg];
-			dispMsg += '<div class="clearfix"><p class="'+message['sender']+'">'+message['content']+'</p></div>';
+		var keys = Object.keys(messages);
+		var dispMsg = "";
+		var emptyBody = $('#msgs-body').html() == "" ? true : false;
+		var prevScrollHeight = $('#msgs-body').prop('scrollHeight');
+
+		// loop inreverse
+		for (var i = keys.length; i-- > 0;) {
+			var message = messages[keys[i]];
+			dispMsg = '<div class="clearfix"><p class="'+message['sender']+' data-date_time="'+message['date_time']+'">'+message['content']+'</p></div>';
+
+			// insert into #msg-body
+			if (emptyBody) {
+				$('#msgs-body').append(dispMsg);
+				emptyBody = false;
+			}
+			else {
+				$(dispMsg).insertBefore('#msgs-body div:first-child');
+			}
 		}
 
-		// insert into #msg-body
-		if (pos == "after") {
-			$('#msgs-body').append(dispMsg);
-
-			// toggle to #messages-view
-			toggleView($('#messages-list'), $('#messages-view'), function() {
-				$('#msgs-body').scrollTop($('#msgs-body').prop('scrollHeight'))
-			});
-		}
-		else if (pos == "before") {
-			var prevScrollHeight = $('#msgs-body').prop('scrollHeight');
-			console.log(prevScrollHeight);
-
-			// insert on top of #msgs-body
-			$(dispMsg).insertBefore('#msgs-body div:first-child');
-
-			// scroll to end of the added messages
-			var newScrollPos = $('#msgs-body').prop('scrollHeight') - prevScrollHeight;
-			console.log($('#msgs-body').prop('scrollHeight'));
-			console.log(newScrollPos);
-			$('#msgs-body').scrollTop(newScrollPos);
-		}
+		// scroll to end of the added messages
+		var newScrollPos = $('#msgs-body').prop('scrollHeight') - prevScrollHeight;
+		$('#msgs-body').scrollTop(newScrollPos);
 	}
 	else {
 		// insert on top of #msgs-body a notice
@@ -90,7 +81,12 @@ $('#messages-list h4').on('click', function() {
 
 	// get and display the messages for the coresponding conversation
 	convId = $(this).parent().data('conv_id');
-	getMessages({convId: convId}, "after");
+	getMessages({convId: convId});
+
+	// toggle to #messages-view
+	toggleView($('#messages-list'), $('#messages-view'), function() {
+		$('#msgs-body').scrollTop($('#msgs-body').prop('scrollHeight'));
+	});
 })
 
 // add previous messages upon reaching the top
@@ -103,9 +99,9 @@ $('#msgs-body').scroll(function() {
 			convId: convId,
 			oldestMsgId: oldestMsgId
 		};
-		
+
 		// prevent successive triggering
-		setTimeout(function() { getMessages(ref, "before"); }, 800);
+		setTimeout(function() { getMessages(ref); }, 800);
 	}
 });
 
